93c1ed0b473979c58baeb9a2a10e7afc2ffae150
azminajaheer <147899557+azminajaheer@users.noreply.github.com>
2024-09-03 17:08:43 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
